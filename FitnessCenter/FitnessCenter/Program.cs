37113fb8e050314236c36442a6d9530a929e1558
Zuhair Ali <szali172@gmail.com>
2024-06-13 19:40:41 -0500

    Added greeting function

--- FitnessCenter/FitnessCenter/Program.cs
+++ FitnessCenter/FitnessCenter/Program.cs
@@ -1,2 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("Hello, Mom!");
+
+
+Console.WriteLine("Whatever I guess. Greeting function needs to be implemented.");
