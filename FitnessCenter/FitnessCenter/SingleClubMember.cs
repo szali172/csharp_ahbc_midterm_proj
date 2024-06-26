27064eb0917d205e2cab997702aed7cd81b44cb8
Ayow9 <alexander.yowell@gmail.com>
2024-06-26 19:34:56 -0400

    throwexception in singleclub class

--- FitnessCenter/FitnessCenter/SingleClubMember.cs
+++ FitnessCenter/FitnessCenter/SingleClubMember.cs
@@ -12,11 +12,6 @@ public class SingleClubMember : Member //come back to this
     }
     public override void CheckIn(Club club)
     {
-        /*
-         Check to see if its assigned to the passed club
-         Otherwise throw exception
-         If they’re at the right club, add this member to the passed in club’s CheckedIn_Members List property
-         */
         assignedClub = club;
         throw new NotImplementedException();
     }
@@ -26,10 +21,11 @@ public class SingleClubMember : Member //come back to this
     }
     public void ChangeClub(Club newClub)
     {
-        assignedClub = newClub;
+        throw new NotImplementedException ();
     }
     public void ChangeMembership()
     {
-        Console.WriteLine("Your club has changed");
+        throw new NotImplementedException();
+        //Console.WriteLine("Your club has changed");
     }
 }
