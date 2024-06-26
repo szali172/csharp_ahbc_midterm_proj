36d7ea0008de6255edc50d402d309dccf1d49c16
iapetroaei <apetroaei1@gmail.com>
2024-06-26 18:23:25 -0400

    Clubadd

--- FitnessCenter/FitnessCenter/Club.cs
+++ FitnessCenter/FitnessCenter/Club.cs
@@ -1,9 +1,42 @@
-﻿
-namespace FitnessCenter;
+﻿namespace FitnessCenter;
 
-public class Club
+class Club
 {
-    //properties
-    //constructs
-    //methods
+    public string Name { get; set; }
+    public string Address { get; set; }
+    public List<Member> CheckedInMembers { get; set; }
+
+    public Club(string name, string address)
+    {
+        Name = name;
+        Address = address;
+        CheckedInMembers = new List<Member>();
+    }
+
+    public void DisplayMemberInfo(Member member)
+    {
+        Console.WriteLine($"ID: {member.Id}, Name: {member.Name}");
+    }
+
+    public void AddMember(Member member)
+    {
+        CheckedInMembers.Add(member);
+    }
+
+    public void RemoveMember(Member member)
+    {
+        CheckedInMembers.Remove(member);
+    }
+
+    public decimal GenerateFees(Member member)
+    {
+        if (member is MultiClubMember)
+        {
+            return 15.0m;
+        }
+        else
+        {
+            return 10.0m;
+        }
+    }
 }
