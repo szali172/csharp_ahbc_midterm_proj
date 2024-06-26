00de02bdb25275852ae35d230e5aff433d4a76e3
Ayow9 <alexander.yowell@gmail.com>
2024-06-26 19:05:49 -0400

    modified member parent class, updated singleclub class

---
+++ FitnessCenter/FitnessCenter/SingleClubMember.cs
@@ -0,0 +1,27 @@
+﻿
+namespace FitnessCenter;
+
+public class SingleClubMember : Member //come back to this
+{
+    private Club assignedClub;
+    //methods
+    public void SingleClub(Club assignedclub, string Name, string ID)
+    {
+        Name = Name;
+        ID = ID;
+    }
+    public override void CheckIn(Club club)
+    {
+        /*
+         Check to see if its assigned to the passed club
+         Otherwise throw exception
+         If they’re at the right club, add this member to the passed in club’s CheckedIn_Members List property
+         */
+        assignedClub = club;
+        throw new NotImplementedException();
+    }
+    public override void CheckOut(Club club)
+    {
+        throw new NotImplementedException();    
+    }
+}
